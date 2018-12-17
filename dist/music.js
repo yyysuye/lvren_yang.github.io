f646cae7e1d13288c1d8c6af58841a3d45cfd7c5
yyysuye <ycxdawn@163.com>
2018-12-17 16:45:32 +0800

    Site updated: 2018-12-17 16:45:31

--- dist/music.js
+++ dist/music.js
@@ -1,7 +1,7 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
 	preload : 'auto',
 	mutex: true,
     audio: [
